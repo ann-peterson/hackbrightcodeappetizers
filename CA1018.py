3b3249024b8e1b69655d34f6a55fcf48f03bc6f2
ann-peterson <annppeterson@icloud.com>
2016-11-18 14:02:51 -0800

    Add files via upload
    10/18/16

---
+++ CA1018.py
@@ -0,0 +1,20 @@
+if ("hello" == "hello" or 3 == 8 or "brighticorn" == "code"):
+	print "welcome to class today!"
+else:
+	print "nothing is equal!"	
+
+x = 3
+if x ==3:
+	x = 4
+	if x == 4:
+		print "x is 4"
+else:
+	x = 10
+	print "x is 10"		
+
+
+input = raw_input("Do you know who Brighticorn is?")
+if (str.lower(input) == "yes"):
+	print "Great!  She is a wonderful mascot"
+else:
+	print "Brighticorn is the Part Time Course Mascot!"	
